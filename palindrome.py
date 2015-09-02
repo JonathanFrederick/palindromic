3ea40ced58d6c26606b8b7662ed9a7a3ac3415fa
Jonathan Frederick <jrfreder@gmail.com>
2015-09-02 13:51:37 -0400

    init palindrome_test.py and update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,14 @@
-"""A program that asks the user for one or more sentences and then lets the user know if it is a palindrome.""
+"""A program that asks the user for one or more sentences and then lets the user know if it is a palindrome."""
+
+def is_palindrome(sentence):
+    # TODO: return True or False if the sentence is or isn't a palindrome
+    pass
+
+
+def main():
+    # TODO: put your input/output code here
+    pass
+
+
+if __name__ == '__main__':
+    main()
