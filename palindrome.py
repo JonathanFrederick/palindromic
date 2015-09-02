081bdbbf00911dd42a2d458626bff65de5db85a6
Jonathan Frederick <jrfreder@gmail.com>
2015-09-02 15:55:00 -0400

    added recursive palindrome checker

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,21 @@
 """A program that asks the user for one or more sentences and then lets the user know if it is a palindrome."""
+import re
 
 def is_palindrome(sentence):
     # TODO: return True or False if the sentence is or isn't a palindrome
-    pass
+    sentence = re.sub(r'[^A-Za-z]', '', sentence.lower())
+    if len(sentence) <= 1:
+        return True
+    elif sentence[0] == sentence[-1]:
+        return is_palindrome(sentence[1:-1])
+    return False
+
 
 
 def main():
     # TODO: put your input/output code here
-    sen = input("Please enter a sentence\n>>>")
+    sen = "No, sir, away. A papaya war is on!"#input("Please enter a sentence\n>>>")
+    #print(re.sub(r'[^A-Za-z]', '', sen.lower()))
     print(is_palindrome(sen))
 
 
